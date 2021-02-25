151ebe756bda05bce443debc64aa9059ccd5df8a
MITU <ysydty@outlook.com>
2021-02-25 14:04:46 +0800

    Push a error about this project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Color-Palette
 It is a Color Palette component written in C# for the WinFrom platform.
+
+## **There is a problem with the current project and is being repaired !**
---
+++ example/ColorPaletteExample/ColorPaletteExample/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using 调色板;
+
+namespace ColorPaletteExample
+{
+    public partial class 调色板示例程序 : Form
+    {
+        public 调色板示例程序()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenColorPalette_Click(object sender, EventArgs e)
+        {
+            调色板.调色板 ColorPalette = new 调色板.调色板();
+            ColorPalette.ShowDialog();
+        }
+    }
+}
